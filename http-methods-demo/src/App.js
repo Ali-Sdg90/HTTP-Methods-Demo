0181c2642c0d6b1c21bf288dc95a750ef4dddd22
Ali Sadeghi <ali909392@gmail.com>
2023-08-17 21:46:48 +0330

    Add toggleble header and title / Small improvment to style / Fix gh-p

--- http-methods-demo/src/App.js
+++ http-methods-demo/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import AxiosGet from "./components/AxiosGet";
 import AxiosPost from "./components/AxiosPost";
@@ -9,9 +9,25 @@ import AxiosPut from "./components/AxiosPut";
 import Footer from "./components/Footer";
 
 const App = () => {
+    const [hedder, setHedder] = useState("- HTTP Methods Demo -");
+
+    useEffect(() => {
+        document.title = hedder;
+    }, [hedder]);
+
+    const handleMouseEnter = () => {
+        setHedder("Axios Methods Demo");
+    };
+
+    const handleMouseLeave = () => {
+        setHedder("HTTP Methods Demo");
+    };
+
     return (
         <div>
-            <h1>- Axios -</h1>
+            <h1 onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                - {hedder} -
+            </h1>
             <AxiosGet />
             <AxiosPost />
             <AxiosDelete />
